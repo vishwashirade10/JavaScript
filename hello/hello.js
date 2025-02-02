8dc7d3f0f39dd3e921bfe59f85287c656cad3f50
vishwashirade10 <officialvishwas1010@gmail.com>
2025-02-02 15:34:46 +0530

    first commit

---
+++ hello/hello.js
@@ -0,0 +1,13 @@
+//printing methods in JS
+
+console.log("hello world.");
+
+//works only node JS
+process.stdout.write("hello world . JS");
+
+//format key : value -> index : value
+console.table({ name: "abc", ID: 123 });
+
+//error handling.
+console.error("Error!");
+console.warn("Warning!");
